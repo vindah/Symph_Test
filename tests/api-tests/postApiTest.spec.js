432d7e114af9ef31d1d662715c3748c9eed71e42
Patrick Eyo <46420104+vindah@users.noreply.github.com>
2024-11-14 22:47:02 +0100

    added test for creating post

--- tests/api-tests/postApiTest.spec.js
+++ tests/api-tests/postApiTest.spec.js
@@ -16,4 +16,19 @@ test.describe('API tests with CRUD operations', () => {
     expect(initialTotalPosts).toBe(100);
     console.log(`Total number of posts: ${initialTotalPosts}`);
   });
+  test('verify that endpoint can create a new post and store its ID', async ({ request }) => {
+    const response = await request.post(`${apiUrl}/posts`, {
+      data: {
+        title: `Patrick's Post`,
+        body: 'This is a test post created for the assessment',
+        userId: 7,
+      },
+    });
+    expect(response.status()).toBe(201);
+
+    const post = await response.json();
+    createdPostId = post.id;
+    expect(createdPostId).toBeDefined();
+    expect(post.title).toBe(`Patrick's Post`);
+  });
 });
