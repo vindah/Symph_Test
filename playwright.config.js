bbadf704a490fd6bcd9117a63cd524ca97d352ac
Patrick Eyo <46420104+vindah@users.noreply.github.com>
2024-11-15 00:40:05 +0100

    updated github actions files and config

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,10 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],         // For GitHub Actions console logs
+    ['html', { open: 'never' }], // For the detailed HTML report
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: process.env.UI_URL,
